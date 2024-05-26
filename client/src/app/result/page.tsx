c0f518d2f54cf808214c6d65ff97308817474462
hisazakura <luthfanarrayyan@gmail.com>
2024-05-26 19:49:39 +0700

    cleanup

--- client/src/app/result/page.tsx
+++ client/src/app/result/page.tsx
@@ -11,7 +11,6 @@ import Markdown from "react-markdown";
 
 export default function UploadPage() {
   const searchParams = useSearchParams();
-  const [resultId, setResultId] = useState<string | null>(null);
   const [result, setResult] = useState<string | undefined>(undefined);
 
   useEffect(() => {
@@ -47,15 +46,6 @@ export default function UploadPage() {
     // if (response.status != 200) return;
     const data = await response.json();
     setResult(data);
-  }
-
-  function onToggleHandler(state: boolean) {
-    if (state)
-      setResult(
-        resultId +
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-      );
-    else setResult(undefined);
   }
 
   return (
