df1ded949eba0be4d27fe5ac30e24326b08acf47
hisazakura <luthfanarrayyan@gmail.com>
2024-05-26 16:09:34 +0700

    integration

--- client/src/app/result/page.tsx
+++ client/src/app/result/page.tsx
@@ -4,10 +4,10 @@ import Button from "@/components/basic/Button";
 import Switch from "@/components/basic/Switch";
 import NoImage from "@/components/placeholder/NoImage";
 import BouncingDotsSpinner from "@/components/spinner/BouncingDotsSpinner";
-import { stat } from "fs";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
+import Markdown from "react-markdown";
 
 export default function UploadPage() {
   const searchParams = useSearchParams();
@@ -16,32 +16,54 @@ export default function UploadPage() {
 
   useEffect(() => {
     const id = searchParams.get("id");
+    const stocks = searchParams.get("stocks");
     if (id) fetchResult(id);
+    else if (stocks) fetchStockResult(stocks);
   }, [searchParams]);
 
   async function fetchResult(id: string) {
-    const response = await fetch("/api/result?id=" + id);
-    if (response.status != 200) return;
+    console.log("Fetching result...");
+    const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_API_ORIGIN + "/upload_and_search?file_name=" + id);
+    // if (response.status != 200) return;
     const data = await response.json();
+    // console.log(data);
 
-    if (!data.result.result) return;
-    setResult(data.result.result);
+    if (!data.search_results) return;
+    setResult(data.search_results);
+  }
+
+  async function fetchStockResult(stocks: string) {
+    console.log("Fetching result...");
+    const stockList = stocks.split(",");
+    const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_API_ORIGIN + "/calculates", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        stock_symbols: stockList,
+      }),
+    });
+    // if (response.status != 200) return;
+    const data = await response.json();
+    setResult(data);
   }
 
   function onToggleHandler(state: boolean) {
     if (state)
       setResult(
-        resultId + "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        resultId +
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       );
     else setResult(undefined);
   }
 
   return (
     <div className="flex flex-col p-12 gap-y-6">
-      <div className="flex flex-row items-center gap-x-2 hidden">
+      {/* <div className="flex flex-row items-center gap-x-2 hidden">
         <Switch onToggle={onToggleHandler} />
         <p className="text-lg">Result is {result == undefined ? "hidden" : "shown"}</p>
-      </div>
+      </div> */}
       <div className="flex justify-center">{}</div>
       <div className="flex flex-row items-center gap-x-4">
         <div className="aspect-square w-[72px]">
@@ -57,7 +79,7 @@ export default function UploadPage() {
             <p className="text-lg">Try refreshing this page after a few minutes.</p>
           </div>
         ) : (
-          <p className="text-lg">{result}</p>
+          <Markdown skipHtml={false}>{result}</Markdown>
         )}
       </div>
       <Link href="/upload">
